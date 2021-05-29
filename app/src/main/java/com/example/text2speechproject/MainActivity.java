66270c186e3ef04b18f8c268f2683bdab022acf8
ilkayalbayrak <ilkayyalbayrak@gmail.com>
2021-05-29 16:33:58 +0200

    Initial commit

---
+++ app/src/main/java/com/example/text2speechproject/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.text2speechproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+}
